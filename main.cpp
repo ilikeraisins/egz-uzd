9e99e86914c7aff67cfc390694b6646aa00a2901
unknown <monemuznikaite@gmail.com>
2025-12-24 19:31:40 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,49 +4,58 @@
 
 int main()
 {   
-    map<string, int> words;
-    std::stringstream buffer = failo_skaitimas("history.txt");
-    string word;
+    int choice;
+    cout << "Dalis: ";
+    std::cin >> choice;
 
-    while (buffer) {
-        if (!buffer.eof()) {
-            buffer >> word;
-            std::locale loc("en_US.UTF-8");
-            
-            
-            while (!word.empty()) {
-                if (std::isalpha(word.front(), loc) == 0) {
-                    word.erase(0, 1);
-                }
-                else if(std::isalpha(word.back(), loc) == 0) {
-                    word.pop_back();
-                }
-                else {
-                    break;
-                 }
-            }
-         
-            if (ar_zodis(word) and word != "") {
-                if (words.find(word) != words.end()) {
-                    words[word] = words[word] + 1;
-                }
-                else {
-                    words.insert({ word,1 });
-                }
-            }
-            
-        }
-        else break;
-    };
+    //map<string, int> words;
+    std::stringstream buffer = read_file("history.txt");
+    std::locale loc("en_US.UTF-8");
 
-    for (auto i : words) {
-        if (i.second > 1) {
-            cout << i.first << " " << i.second << endl;
+    if (choice == 1) {
+        map<string, int> map;
+        string word;
+
+        while (buffer) {
+            if (!buffer.eof()) {
+                buffer >> word;
+
+
+
+                while (!word.empty()) {
+                    if (std::isalpha(word.front(), loc) == 0) {
+                        word.erase(0, 1);
+                    }
+                    else if (std::isalpha(word.back(), loc) == 0) {
+                        word.pop_back();
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+
+                if (word != "") {
+                    if (map.find(word) != map.end()) {
+                        map[word] = map[word] + 1;
+                    }
+                    else {
+                        map.insert({ word,1 });
+                    }
+                }
+
+            }
+            else break;
+        };
+
+        for (auto i : map) {
+            if (i.second > 1) {
+                cout << i.first << " " << i.second << endl;
+            }
+
         }
-        
+
+        //write_file(map, "map.txt");
     }
-
-    //failo_isvedimas(words,"sutvarkyti.txt");
-
 
 }
